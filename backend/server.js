4c0dd6add89cb53c8a1014d177b6be3b92507558
tasin-2019331009 <tahsinul09@student.sust.edu>
2024-12-27 19:17:44 +0600

    both node_module deleted

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
-import {connectDB} from './config/db.js';
+import { connectDB } from "./config/db.js";
 import productRouter from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json()); //allows us to accept json data in req.body
 
 app.use("/api/products", productRouter);
 
-app.listen(PORT, ()=>{
-    connectDB();
-    console.log(`Server is listening to port ${PORT} at http://localhost:5000`);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is listening to port ${PORT} at http://localhost:5000`);
 });
